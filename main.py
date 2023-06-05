058bff067d7bdab592bdc0689ebaf6d6267effb8
Muhammad Naufal Rafif Hibatullah <naufif17@gmail.com>
2023-06-05 14:05:41 +0700

    memperbaiki data tabel

--- main.py
+++ main.py
@@ -25,9 +25,14 @@ async def input(ctx, *, input_data):
         await ctx.send(f'Data yang Anda masukkan: {input_data}')
         response = requests.get(url + input_data)
         data_json = response.json()
-        df = pd.DataFrame(data_json)
-
-        table = tabulate(df, headers='keys', tablefmt='fancy_grid')
+        df = pd.DataFrame(data_json['matches'])
+        df['result'] = df['details'].apply(lambda x: x['result'])
+        df['date'] = df['details'].apply(lambda x: x['date'])
+        df['kills'] = df['details'].apply(lambda x: x['kills'])
+        df['deaths'] = df['details'].apply(lambda x: x['deaths'])
+        df['assists'] = df['details'].apply(lambda x: x['assists'])
+        df['gpm'] = df['details'].apply(lambda x: x['gpm'])
+        table = tabulate(df[['match_id', 'kills', 'deaths', 'assists', 'gpm', 'result', 'date']], headers='keys', tablefmt='fancy_grid')
 
         with open('table.txt', 'w', encoding="utf-8") as f:
             f.write(table)
